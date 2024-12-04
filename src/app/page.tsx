0101330f07a62ace6fbdfb340de6d18ce7c9858f
rayhanalmim <rayhanalmim1@gmail.com>
2024-12-04 15:16:48 +0600

    Add wallet connection and payment functionality with Web3Modal integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
-import AddItem from "@/components/page/Modal/AddItem";
-import Header from "@/components/shared/Header";
-import Image from "next/image";
+"use client";
+import WalletConnect from "src/components/WalletConnect";
+import PaymentForm from "src/components/PaymentForm";
+import { useState } from "react";
+import { ethers } from "ethers";
 
 export default function Home() {
+
+  const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>(null);
+
+  const handleWalletConnect = (connectedProvider: ethers.providers.Web3Provider | null) => {
+    setProvider(connectedProvider);
+  };
+
+
   return (
     <div >
-      <Header />
+      <h3>hello this in the client side</h3>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-2xl font-bold mb-8">Blockchain Payment App</h1>
+        <div className="min-h-screen flex flex-col items-center justify-center space-y-8">
+          <WalletConnect onWalletConnect={handleWalletConnect} />
+          <PaymentForm provider={provider} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const AVALANCHE_FUJI_RPC = 'https://api.avax-test.network/ext/bc/C/rpc';
+export const RECEIVER_ADDRESS = '0x35ED7A3410228359E9dc53D75d3038B910E33D06';
